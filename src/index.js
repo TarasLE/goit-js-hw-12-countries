565b9286957a9d60b599869cfa3a4cda023f8a05
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-14 17:01:02 +0200

    interimsave

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles.css';
+
+const refs = {
+    searchForm: document.querySelector('js-search-form'),
+    countriesContainer: document.querySelector('js-countries-container')
+}
+
+refs.searchForm.addEventListener('change', onSearch)
+
+
+function onSearch(event) {
+    event.preventDefault();
+
+    const searchQuery = event.currentTarget.element.query.value
+
+    const option = {
+        headers: {}
+    }
+
+    const url = 'https://restcountries.eu/rest/v2/name'
+
+    fetch(url, option).then(response => response.json()).then(console.log)
+    
+}
