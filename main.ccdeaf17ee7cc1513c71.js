77636b19d85804c07f95dd2a5b1b745862f587f7
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-14 19:26:00 +0200

    Updates

--- main.ccdeaf17ee7cc1513c71.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{QfWi:function(e,n,t){"use strict";t.r(n);t("JBxO"),t("FdtR");({searchForm:document.querySelector(".js-search-form"),countriesContainer:document.querySelector(".js-countries-container")}).searchForm.addEventListener("input",_.debounce((function(e){e.preventDefault();var n=e.target.value;console.log(n),fetch("https://restcountries.eu/rest/v2/name/"+n,{headers:{}}).then((function(e){return e.json()})).then(console.log)}),500))}},[["QfWi",1,2]]]);
-//# sourceMappingURL=main.ccdeaf17ee7cc1513c71.js.map
