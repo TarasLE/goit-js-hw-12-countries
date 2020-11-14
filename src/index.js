8906a5b7574147c0cbcfbece5e0a72da826eaf8c
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-14 22:16:04 +0200

    save

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 // import 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js'
 import '../node_modules/lodash/lodash'
 import countriesListTpl from './templates/countries-list.hbs'
+import desiranbleCountryTpl from './templates/desirableCountry.hbs'
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
 
+const { error } = require('@pnotify/core');
 const refs = {
     searchForm: document.querySelector('.js-search-form'), 
     countriesContainer: document.querySelector('.js-countries-container')
@@ -26,7 +30,19 @@ function onSearch(event) {
     const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`
 
     // fetch(url, option).then(response => response.json()).then(console.log)
-    return fetch(url, option).then(response => response.json()).then(data => data).then(appendCountriesList)
+    return fetch(url, option).then(response => response.json()).
+        then(data => {
+            console.log(data.length);
+            if (data.length > 10) {
+                return error({
+                    text: 'Too many matches found. Please try a more specific query',
+                    type: 'info'
+                })
+            }
+            else if (data.length === 1) { renderDesirableCountry(data); }
+            else { appendCountriesList(data) }
+        }).
+        then(appendCountriesList)
     // debounce(500)
 }
 
@@ -34,9 +50,15 @@ function appendCountriesList(countries) {
     refs.countriesContainer.insertAdjacentHTML("beforeend",countriesListTpl(countries))
 }
 
+function renderDesirableCountry(country) {
+    refs.countriesContainer.insertAdjacentHTML("beforeend",desiranbleCountryTpl(country))
+}
+
 function clearCountriesList() {
     refs.countriesContainer.innerHTML = ''
 }
+
+
 // console.log('test1');
 
 // const url = 'https://restcountries.eu/rest/v2/name/Colombia'
