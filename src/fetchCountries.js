eb8add6365e07614043f1754fc30586e8cafe993
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-15 15:26:20 +0200

    save

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -14,3 +14,43 @@ export default function fetchCountries(event) {
              console.log('Error fetching data');
         })
 }
+
+function appendCountriesList(countries) {
+    refs.countriesContainer.insertAdjacentHTML("beforeend",countriesListTpl(countries))
+}
+
+function renderDesirableCountry(country) {
+    refs.countriesContainer.insertAdjacentHTML("beforeend",desiranbleCountryTpl(country))
+}
+
+function clearCountriesList() {
+    refs.countriesContainer.innerHTML = '';
+}
+
+function findDesirableCountry(data) {
+    
+    if (data.length > 10) {
+            error({
+                   delay: 1000,
+                    text: 'Too many matches found. Please try a more specific query',
+                    type: 'info'
+            })
+       
+        
+            }
+            else if (data.length === 1) {
+                renderDesirableCountry(data);
+            }
+    else if (data.length === undefined) {
+           error({
+                delay: 1000,
+                text: 'Incorrect name of the counrty. Please check and try again',
+                type: 'info'
+                });
+                            
+            }
+            else {
+               appendCountriesList(data);
+            }
+}
+
--- src/index.js
+++ src/index.js
@@ -18,43 +18,4 @@ refs.searchForm.addEventListener('input', _.debounce(fetchCountries,500))
 let searchQuery = '';
  
 
-function appendCountriesList(countries) {
-    refs.countriesContainer.insertAdjacentHTML("beforeend",countriesListTpl(countries))
-}
-
-function renderDesirableCountry(country) {
-    refs.countriesContainer.insertAdjacentHTML("beforeend",desiranbleCountryTpl(country))
-}
-
-function clearCountriesList() {
-    refs.countriesContainer.innerHTML = '';
-}
-
-function findDesirableCountry(data) {
-    
-    if (data.length > 10) {
-            error({
-                   delay: 1000,
-                    text: 'Too many matches found. Please try a more specific query',
-                    type: 'info'
-            })
-       
-        
-            }
-            else if (data.length === 1) {
-                renderDesirableCountry(data);
-            }
-    else if (data.length === undefined) {
-           error({
-                delay: 1000,
-                text: 'Incorrect name of the counrty. Please check and try again',
-                type: 'info'
-                });
-                            
-            }
-            else {
-               appendCountriesList(data);
-            }
-}
-
 
